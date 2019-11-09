4cea5c2652c572cfdfb8a3df056204069d206fa8
Efen Stakes <efenstakes101@gmail.com>
2019-11-09 22:02:16 +0300

    updated to use express

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
-const http = require('http');
+// const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = process.env.PORT || 5000;
+// const hostname = '127.0.0.1';
+// const port = process.env.PORT || 5000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World, heroku testing\n');
-});
-
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`)
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World, heroku testing\n');
 // });
-server.listen(port, () => {
-    console.log(`Server running at ${port}/`)
-  });
+
+// // server.listen(port, hostname, () => {
+// //   console.log(`Server running at http://${hostname}:${port}/`)
+// // });
+// server.listen(port, () => {
+//     console.log(`Server running at ${port}/`)
+//   });
+
+const express = require('express')
+
+app = express()
+
+const PORT = process.env.PORT || 5000
+
+
+app.get('/', (req, res)=> {
+    res.json({ 'page': 'index', 'status': 'indexing' })
+})
+
+app.get('/about', (req, res)=> {
+    res.json({ 'page': 'about', 'status': 'abouting' })
+})
+
+app.get('/services', (req, res)=> {
+    res.json({ 'page': 'services', 'status': 'chilling' })
+})
+
+
+app.listen(PORT, ()=> {
+    console.log(`running app on port ${PORT}`)
+})
